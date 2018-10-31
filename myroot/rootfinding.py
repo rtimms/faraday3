f7301cf3efdb8c7694d435a8b594f9cf12bc551a
Robert Timms <timms@maths.ox.ac.uk>
2018-10-31 11:16:34 +0000

    added rootfinding.py

---
+++ myroot/rootfinding.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def myfunc(x):
+    return np.sin(x**2) - x + 5
+
+
+def mybisection(myfunc, a, b, epsilon):
+
+    if abs(myfunc(a)) < epsilon:
+        print('a is a root!')
+        return a
+
+    if abs(myfunc(b)) < epsilon:
+        print('b is a root!')
+        return b
+
+    while abs(b-a) > epsilon:
+
+        m = a + (b - a)/2
+        print(m)
+
+        if myfunc(a)*myfunc(m) < 0:
+            b = m
+        else:
+            a = m
+
+    return m
+
+
+m = mybisection(myfunc, 3.8, 6, 0.001)
+
+x = np.linspace(m - 1, m + 1 , 100)
+plt.plot(x, myfunc(x))
+plt.plot(m, 0, 'bo')
+plt.grid()
+plt.show()
